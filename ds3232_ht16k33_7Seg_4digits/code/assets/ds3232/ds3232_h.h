3ff3571a17f0b720e524a59c56a77635cb81f4ec
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2025-12-19 14:15:49 +0200

    Add files via upload

---
+++ ds3232_ht16k33_7Seg_4digits/code/assets/ds3232/ds3232_h.h
@@ -0,0 +1,35 @@
+#ifndef DS3232_H
+#define	DS3232_H
+//------------------------------------------------------------------------------
+#include "../i2c/soft_i2c.h"
+//------------------------------------------------------------------------------
+typedef struct 
+{
+  int  second;     //second (0-59)
+  int  minute;     //minute (0-59)
+  int  hour;       //hour (0-23)
+  int  dayOfWeek;  //day of the week (1-7, 1=Sunday)
+  int  dayOfMonth; //month (1-31)
+  int  month;      //month (1-12)
+  int  year;       //year (e.g., 2023)
+} time_t;
+//------------------------------------------------------------------------------
+#define WR 0
+#define RD 1
+//------------------------------------------------------------------------------
+#define tempMSB_REG            0x11
+#define tempLSB_REG            0x12
+#define FRAC_FLAG		 	         0x00C0
+//------------------------------------------------------------------------------
+void ds3232_init(uint8_t time[]);
+uint8_t ds3232_read(uint8_t addr);
+void ds3232_setDateTime(uint8_t time[]);
+void ds3232_getDateTime(time_t *tm, uint8_t time[]);
+void ds3232_setDateTimeObj(time_t *tm, uint8_t time[]);
+uint16_t ds3232_getRawTemp(void);
+float ds3232_getTemp(void);
+float toFloat(int16_t rawData);
+uint8_t calc_decTime(uint8_t time);
+uint8_t calc_bcdTime(uint8_t time);
+//------------------------------------------------------------------------------
+#endif
---
+++ ds3232_ht16k33_7Seg_4digits/code/assets/ht16k33/ht16k33_c.h
@@ -0,0 +1,136 @@
+#ifndef HT16K33_C
+#define	HT16K33_C
+//------------------------------------------------------------------------------
+#include "ht16k33_h.h"
+//------------------------------------------------------------------------------
+void ht16k33_init(uint8_t brightness)
+{     
+  i2c_init();           
+	
+  delay_ms(5);               // Required after Power On reset to give time to initialize device
+ 
+  ht16k33_on();
+  ht16k33_setBrightness(brightness);   // Valid values from 0 - 15
+  ht16k33_blink(0);            // Blink OFF
+}
+//------------------------------------------------------------------------------
+void ht16k33_on()
+{
+  i2c_start();
+	
+  i2c_write(HT16K33_ADDRESS);
+  i2c_write(OSCILLATOR_ON);          
+	
+  i2c_stop();
+}
+//------------------------------------------------------------------------------
+void ht16k33_setBrightness(uint8_t num)
+{   
+  uint8_t level = ( num <= 15 ) ? num : 1;
+	
+  i2c_start();
+ 
+  i2c_write(HT16K33_ADDRESS);
+  i2c_write(DIGITAL_DIM | level);  // Dimming command. valid values are 0 - 15
+ 
+  i2c_stop();
+}
+//------------------------------------------------------------------------------
+void ht16k33_blink(uint8_t data)
+{
+  i2c_start();
+	
+  i2c_write(HT16K33_ADDRESS);
+  i2c_write(DISPLAY_SETUP | (data << 1));  // Blinking / blanking command
+	
+  i2c_stop();
+}
+//------------------------------------------------------------------------------
+void ht16k33_print(const char* format, ...)
+{
+	char str[40];
+	
+	va_list args; // Declare a va_list variable
+	va_start(args, format); // Initialize the va_list with the last fixed argument
+
+	// Use vsprintf to print with the provided format and variadic arguments
+	vsprintf(str, format, args); 
+
+	va_end(args); // Clean up the va_list
+	
+  ht16k33_string(str);
+}
+//------------------------------------------------------------------------------
+void ht16k33_string(char text[])
+{
+  i2c_start();
+	
+  i2c_write(HT16K33_ADDRESS);
+  i2c_write(DISPLAY_MEMORY);     // Display data start address. Auto increments on every write.
+  ht16k33_write(text);
+	
+  i2c_stop();
+}
+//------------------------------------------------------------------------------
+void ht16k33_write(char text[])
+{
+  uint8_t chrPos = 0;
+  int8_t dpPos[10] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};	
+		
+  setDpPos(dpPos, text);
+	
+  while(*text)
+  {
+		uint8_t chr = *text++;
+		
+		if(chr != '.' && chr != 194)		//° is implicitly split into 194 and 176 by the arduino c++ compiler
+		{
+			writeChar(chr, chrPos, dpPos[chrPos]);
+			chrPos++;
+		}
+  }
+}
+//------------------------------------------------------------------------------
+void writeChar(char chr, uint8_t chrPos, int8_t dpPos)
+{	
+  uint8_t digit = getDigit(chr, chrPos, dpPos); //ascii to 7 seg
+
+  i2c_write(digit);			// Lower byte  (D7 - D0)
+  i2c_write(0);					// Higher byte (D15 - D8) Unused in 7 seg display
+}
+//------------------------------------------------------------------------------
+//          				ascii to 7 segment
+//------------------------------------------------------------------------------
+uint8_t getDigit(char chr, uint8_t chrPos, int8_t dpPos)
+{
+  uint8_t digit = 0;
+
+  digit = (isPrint(chr)) ? chars[chr - ' '] : custom_chars[0];  // custom char is ° symbol
+	
+  return (dpPos != -1 && chrPos == dpPos) ? digit + DECIMAL_POINT : digit; 
+}
+//------------------------------------------------------------------------------
+void setDpPos(int8_t dpPos[], char chars[])
+{
+  uint8_t i, pos = 0; 
+	
+  for(i = 0; chars[i]; i++)
+  {
+    if(chars[i] == '.')
+		{
+			dpPos[pos-1] = pos - 1;
+		}
+		else
+				{
+					pos++;
+				}
+	}
+}
+//------------------------------------------------------------------------------
+inline uint8_t isPrint(char chr)
+{
+	return (chr >= 32 && chr <= 127) ? 1 : 0;
+}
+//------------------------------------------------------------------------------
+#endif	/* HT16K33_C */
+
