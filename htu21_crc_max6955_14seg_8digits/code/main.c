f2bc2e418d9334b13442668d723423bf671dfe1a
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2025-12-19 10:31:40 +0200

    Add files via upload

---
+++ htu21_crc_max6955_14seg_8digits/code/main.c
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdint.h>
+#include <stm32f4xx.h>
+#include "./assets/gpio/gpio_c.h"
+#include "./assets/delay/delay_c.h"
+#include "./assets/htu21/htu21_c.h"
+#include "./assets/max6955/max6955_c.h"
+//------------------------------------------------------------------------------
+char str[40];
+uint16_t rawHumd, rawTemp;
+//------------------------------------------------------------------------------
+int main(void)
+{	  
+	HSI_init();					// Set Clock to 16Mhz	
+	max6955_init(1);		// Number of chips connected in parallel
+	htu21_init(RES_3);	// Internal pull-up is enabled within the gpio configurations, hence no external resistor
+
+	while(1)
+	{		 
+		rawHumd = htu21_readConv(HUMD_NO_HOLD_CMD);
+		rawTemp = htu21_readConv(TEMP_NO_HOLD_CMD);
+		
+		sprintf(str, "%8.1f%c", htu21_calcHumd(rawHumd), '%'); 
+		//sprintf(str, "%7.1f°C", htu21_calcTemp(rawTemp)); 
+		max6955_displayText(str);
+		
+		delay_ms(200);
+	}
+}
+//------------------------------------------------------------------------------
+
