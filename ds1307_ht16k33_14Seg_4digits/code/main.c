d470e9932ece4a172cd5e6f0d67d049eafcc8360
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2025-12-28 16:45:47 +0200

    Add files via upload

---
+++ ds1307_ht16k33_14Seg_4digits/code/main.c
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdint.h>
+#include <stm32f4xx.h>
+#include "./assets/gpio/gpio_c.h"
+#include "./assets/delay/delay_c.h"
+#include "./assets/ds1307/ds1307_c.h"
+#include "./assets/ht16k33/ht16k33_c.h"
+//------------------------------------------------------------------------------
+uint8_t time[8] = { 57, 34, 9, 1, 2, 12, 25 }; // Sec,Min,Hour,WDay,MDay,Month,Year
+time_t tm;
+uint8_t isColonOn = 0;
+//------------------------------------------------------------------------------
+int main(void)
+{	  
+	HSI_init();				 	// Set Clock to 16Mhz	
+	ht16k33_init(15);   // Initialize HT16K33 with brightness level 15 
+	ds1307_init(time);  //Initialize DS3232 RTC with initial time
+
+	while(1)
+	{
+		ds1307_getDateTime(&tm, time); 
+		
+		if(isColonOn)
+			ht16k33_print("%02d%02d", tm.hour, tm.minute);
+		else
+			ht16k33_print("%02d.%02d", tm.hour, tm.minute);
+
+		isColonOn = 1 - isColonOn; // toggle state;
+		delay_ms(1000);
+	}
+}
+//------------------------------------------------------------------------------
+
