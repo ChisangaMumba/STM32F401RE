dfe3cb93a1b14ee7a296f5df3751caa53f18a47e
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2025-12-28 18:10:00 +0200

    Add files via upload

---
+++ dht11_mcp23s17_7seg_2digits/code/assets/mcp23s17/mcp23s17_h.h
@@ -0,0 +1,32 @@
+#ifndef MCP23S17_H
+#define MCP23S17_H
+//------------------------------------------------------------------------------
+#include "../spi/soft_spi.h"
+#define CS1   PA3
+//-----------------------I/O Direction Registers(TRIS)--------------------------
+#define IODIR_A    0x00
+#define IODIR_B    0x01
+//-----------------------General Purpose I/O------------------------------------
+#define GPIO_A     0x12   //Reading the GPIOn register reads the value on the port.
+#define GPIO_B     0x13
+//-----------------------Output Latches-----------------------------------------
+#define OLAT_A     0x14   //Writing to the OLATn register Writes to the associated port.
+#define OLAT_B     0x15
+//-----------------------W/R Control bytes--------------------------------------
+#define WRITE      0x40   // 0b0100 + 0b000(Slave address) + 0b0(Write) => 0x40
+#define READ       0x41
+//---------------------------Input pin Bit postions-----------------------------
+#define I0         0x01
+#define I1         0x02
+#define I2         0x04
+#define I3         0x08
+#define I4         0x10
+#define I5         0x20
+#define I6         0x40
+#define I7         0x80
+//------------------------------------------------------------------------------
+void    mcp23s17_init(uint8_t portStateA, uint8_t portStateB);
+void    mcp23s17_write(uint8_t regAddr, uint8_t data);
+uint8_t mcp23s17_read(uint8_t port);               
+//------------------------------------------------------------------------------
+#endif
---
+++ dht11_mcp23s17_7seg_2digits/code/assets/sevenSeg/sevenSeg_c.h
@@ -0,0 +1,102 @@
+#ifndef SEVEN_SEGMENT_C
+#define	SEVEN_SEGMENT_C
+//------------------------------------------------------------------------------
+#include "sevenSeg_h.h"
+//------------------------------------------------------------------------------
+void sevenSeg_init(void)
+{
+	mcp23s17_init(0x00, 0x00);
+	sevenSeg_string("  ");
+}
+//------------------------------------------------------------------------------
+void sevenSeg_print(const char* format, ...) 
+{
+	char str[30];
+	
+	va_list args; // Declare a va_list variable
+	va_start(args, format); // Initialize the va_list with the last fixed argument
+
+	// Use vsprintf to print with the provided format and variadic arguments
+	vsprintf(str, format, args); 
+
+	va_end(args); // Clean up the va_list
+	
+	sevenSeg_string(str);
+}
+//------------------------------------------------------------------------------
+void sevenSeg_string(char str[])
+{  
+	uint8_t chrPos = 0,  digit[10];
+	int8_t dpPos[10] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+	
+	setDpPos(dpPos, str);
+	
+  while(*str)
+  {
+		uint8_t chr = *str++;
+		
+		if(chr != '.' && chr != 194)  // ascii 194 is an extra character created by the degree ° symbol
+    {
+			digit[chrPos] = getDigit(chr, chrPos, dpPos[chrPos]);
+			chrPos++;
+		} 
+  }
+	
+	mcp23s17_write(OLAT_A, digit[1]);
+	mcp23s17_write(OLAT_B, digit[0]);
+}
+//------------------------------------------------------------------------------
+//													ascii to 7 segment
+//------------------------------------------------------------------------------
+uint8_t getDigit(char chr, uint8_t chrPos, int8_t dpPos)
+{
+	uint8_t digit = 0;
+	
+	digit = (isDigit(chr)) ? digits[chr - '0'] : getCustomChar(chr);
+					
+	return (dpPos != -1 && chrPos == dpPos) ? (digit + DECIMAL_POINT)  : digit;
+}
+//------------------------------------------------------------------------------
+void setDpPos(int8_t dpPos[], char chars[])
+{
+	uint8_t i, pos = 0; 
+	
+	for(i = 0; chars[i]; i++)
+  {
+    if(chars[i] == '.')
+		{
+			dpPos[pos-1] = pos - 1;
+		}
+		else
+				{
+					pos++;
+				}
+  }
+}
+//------------------------------------------------------------------------------
+uint8_t getCustomChar(char chrAscii)
+{
+	uint8_t chrCode = 0x00; /* space char by default*/
+  
+	for(uint8_t i = 0; customChar[0][i]; i++)
+	{
+		if(toUpper(chrAscii) == customChar[0][i])
+		{
+			chrCode = customChar[1][i]; break;
+		}
+	}	
+					
+	return chrCode;			
+}
+//------------------------------------------------------------------------------
+inline char toUpper(char chr)
+{	
+	return (chr >= 'a' && chr <= 'z') ? (chr - 'a' + 'A') : chr;
+}
+//------------------------------------------------------------------------------
+inline uint8_t isDigit(char chr)
+{
+	return (chr >= '0' && chr <= '9') ? 1 : 0; //Standard ascii range 32 - 127 for printable chars 
+}
+//------------------------------------------------------------------------------
+#endif
