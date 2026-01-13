926bed9db3d683203e0b68f4306adc458837a221
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-13 13:28:33 +0200

    Add files via upload

---
+++ tc72_mcp23sxx_14seg/code/Objects/gpio.d
@@ -0,0 +1,11 @@
+.\objects\gpio.o: assets\gpio\gpio.c
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f401xe.h
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Keil_v5\ARM\ARMCompiler_506_Windows_x86_b960\Bin\..\include\stdint.h
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\gpio.o: C:\Users\chisa\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
+.\objects\gpio.o: assets\gpio\gpio.h
---
+++ tc72_mcp23sxx_14seg/code/assets/fourteenSeg/fourteenSeg_h.h
@@ -0,0 +1,25 @@
+#ifndef FOURTEEN_SEGMENT_H
+#define	FOURTEEN_SEGMENT_H
+//------------------------------------------------------------------------------
+#include <stdarg.h> // Required for variadic functions
+#include "../spi/soft_spi.h"
+#include "../delay/delay_c.h"
+#include "../mcp23s08/mcp23s08_c.h"
+#include "../mcp23s17/mcp23s17_c.h"
+#include "charset.h"
+//------------------------------------------------------------------------------
+#define DIGIT_ADDR				0x40		// mcp23s08 i/o expander for digit data
+#define COLUMN_ADDR				0x42		// mcp23s08 i/o expander for column select
+//------------------------------------------------------------------------------
+void 		fourteenSeg_init(void);
+void 		fourteenSeg_print(const char* format, ...);
+void 		fourteenSeg_string(char text[]);
+void 		writeChar(char chr, uint8_t chrPos, int8_t dpPos);
+uint16_t getDigit(char chr, uint8_t chrPos, int8_t dpPos);
+void		setDpPos(int8_t dpPos[], char chars[]);
+inline	uint8_t isPrint(char chr);
+void 		serial_digitSend(uint16_t digit, uint8_t chrPos);
+//------------------------------------------------------------------------------
+#define DECIMAL_POINT 0x4000
+//------------------------------------------------------------------------------
+#endif
