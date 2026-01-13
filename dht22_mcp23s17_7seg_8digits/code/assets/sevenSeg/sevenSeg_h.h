59a42080fa464ab477fc675ad1a29c2b21b1b0ae
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-13 12:55:36 +0200

    Add files via upload

---
+++ dht22_mcp23s17_7seg_8digits/code/assets/sevenSeg/sevenSeg_h.h
@@ -0,0 +1,30 @@
+#ifndef SEVEN_SEGMENT_H
+#define	SEVEN_SEGMENT_H
+//------------------------------------------------------------------------------
+#include <stdarg.h> // Required for variadic functions
+#include "../mcp23s17/mcp23s17_c.h"
+//------------------------------------------------------------------------------
+void 		sevenSeg_init(void);
+void 		sevenSeg_print(const char* format, ...);
+void 		sevenSeg_string(char str[]);
+void 		writeChar(char chr, uint8_t chrPos, int8_t dpPos);
+uint8_t getDigit(char chr, uint8_t chrPos, int8_t dpPos);
+void		setDpPos(int8_t dpPos[], char chars[]);
+uint8_t getCustomChar(char chrAscii);
+inline char toUpper(char chr);
+inline uint8_t isDigit(char chr);
+
+//------------------------------------------------------------------------------
+#define DECIMAL_POINT 0x80
+//------------------------------------------------------------------------------
+//     Common Anode					 0,    1,    2,    3,    4,    5,    6,    7,    8,   9
+//------------------------------------------------------------------------------
+const char digits[10] = { 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90 };
+//----------------------------Custom characters---------------------------------
+const char customChar[2][10] = 
+{
+	{  '-',  ' ',  '°',  'C',  'F',  'P',  'E',  'O',  'R', '\0' },
+	{ 0xBF, 0xFF, 0x9C, 0xC6, 0x8E, 0x8C, 0x86, 0xC0, 0xCE, '\0' }  // character code
+};
+//------------------------------------------------------------------------------
+#endif
