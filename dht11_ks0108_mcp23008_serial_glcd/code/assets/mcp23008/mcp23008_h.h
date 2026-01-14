15871b5077fc8dfad648c58d668cd8e9eaf73a52
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-14 18:18:46 +0200

    Add files via upload

---
+++ dht11_ks0108_mcp23008_serial_glcd/code/assets/mcp23008/mcp23008_h.h
@@ -0,0 +1,16 @@
+#ifndef MCP23008_H
+#define MCP23008_H
+//------------------------------------------------------------------------------
+#include "../i2c/i2c_c.h"
+//-----------------------I/O Direction Registers(TRIS)--------------------------
+#define IODIR	    			0x00
+//-----------------------General Purpose I/O------------------------------------
+#define GPIO	     			0x09   //Reading the GPIOn register reads the value on the port.
+//-----------------------Output Latches-----------------------------------------
+#define OLAT	     			0x0A   //Writing to the OLATn register Writes to the associated port.
+//------------------------------------------------------------------------------
+void    mcp23008_init(uint8_t devAddr, uint8_t portStateB);
+void    mcp23008_write(uint8_t devAddr, uint8_t regAddr, uint8_t data);
+uint8_t mcp23008_read(uint8_t devAddr);               
+//------------------------------------------------------------------------------
+#endif
