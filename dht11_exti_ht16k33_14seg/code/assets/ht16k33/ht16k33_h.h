d6994c839101842441cb537d42128cd0e091eb48
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-14 17:34:50 +0200

    Add files via upload

---
+++ dht11_exti_ht16k33_14seg/code/assets/ht16k33/ht16k33_h.h
@@ -0,0 +1,28 @@
+#ifndef HT16K33_H
+#define	HT16K33_H
+//------------------------------------------------------------------------------
+#include <stdarg.h> // Required for variadic functions
+#include "../i2c/soft_i2c.h"
+#include "../delay/delay_c.h"
+#include "charset.h"
+//------------------------------------------------------------------------------
+#define HT16K33_ADDRESS  0xE0      //Address bits can be altered on device if wished
+#define OSCILLATOR_ON    0x21      //HT16K33 internal oscillator
+#define DIGITAL_DIM      0xE0      
+#define DISPLAY_SETUP    0x81
+#define DISPLAY_MEMORY   0x00      //Start address. auto increments on every write. valid from 0x00 - 0xFF then auto wraps after last valid address
+#define DECIMAL_POINT    0x4000
+//------------------------------------------------------------------------------
+void    ht16k33_init(uint8_t brightness);
+void    ht16k33_on(void);
+void    ht16k33_blink(uint8_t _data);
+void    ht16k33_setBrightness(uint8_t _data);
+void 		ht16k33_print(const char* format, ...);
+void    ht16k33_string(char text[]);
+void    ht16k33_write(char text[]);
+void    writeChar(char chr, uint8_t chrPos, int8_t dpPos);
+uint16_t getDigit(char chr, uint8_t chrPos, int8_t dpPos);
+void    setDpPos(int8_t dpPos[], char chars[]);
+inline  uint8_t isPrint(char chr);
+//------------------------------------------------------------------------------
+#endif	/* HT16K33_H */
---
+++ dht11_exti_ht16k33_14seg/code/main.c
@@ -0,0 +1,39 @@
+//---------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "./assets/gpio/gpio_c.h"
+#include "./assets/delay/delay_c.h"
+#include "./assets/exti/extInterrupt_c.h"
+#include "./assets/bargraph/bargraph_c.h"
+#include "./assets/dht11/dht11_c.h"
+#include "./assets/ht16k33/ht16k33_c.h"
+//------------------------------------------------------------------------------
+dht11_t sensor;
+//------------------------------------------------------------------------------
+int main(void)
+{			
+	clock_init();	// Set Clock to 16Mhz	
+	ht16k33_init(15);   // Initialize HT16K33 with brightness level 15 
+	bargraph_init();
+	extInt_init(PA6, RISING_EDGE);
+	
+	while(1)
+	{
+		if(dht11_getSensorData(&sensor))
+		{
+			if(isTempMode == 0)
+				ht16k33_print("%7u%c", sensor.humidity, '%');
+			
+			if(isTempMode == 1)
+				ht16k33_print("%6i°C", sensor.temperature);
+	
+			bargraph_write((isTempMode) ? sensor.temperature : sensor.humidity);
+		}
+		else
+				ht16k33_print("%8s", "Error");
+		
+		delay_ms(2000);
+	}
+}
+//------------------------------------------------------------------------------
