2fb1c9a336ee149a41dc7595c7a79bdc13df4fa2
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-14 17:55:19 +0200

    Add files via upload

---
+++ dht22_exti_ht16k33_16seg/code/assets/exti/extInterrupt_c.h
@@ -0,0 +1,95 @@
+#ifndef EXTINT_C
+#define	EXTINT_C
+//------------------------------------------------------------------------------
+#include <stdbool.h>
+#include "../gpio/gpio_c.h"
+//------------------------------------------------------------------------------
+uint8_t isTempMode = 0;
+//------------------------------------------------------------------------------
+#define RISING_EDGE 	0
+#define FALLING_EDGE 	1
+//------------------------------------------------------------------------------
+void extInt_init(uint8_t portPin, uint8_t trigger_mode)
+{ 
+	uint8_t port = portPin / 16;
+	uint8_t pin = portPin % 16;
+	GPIO_TypeDef * GPIO = getGPIO(port);
+	
+	// Enable System Config clock
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	
+	// RCC AHB1 peripheral clock enable register
+	RCC->AHB1ENR |= (0x01UL << port);
+	
+	// Mode Register (MODER): input, output, Alt function, analogue
+	GPIO->MODER &= ~(0x03UL << (pin*2));	// Resetting the value of the bits
+	GPIO->MODER |= INPUT << (pin*2); 		// Setting up bit value to the mode
+	
+	// External Interrupt Configuration Register (EXTICRx)
+	uint8_t confReg = pin / 4;		// EXTICR0, EXTICR1, EXTICR2, EXTICR3
+	uint8_t confPos = pin % 4;		// 0,1,2,3 e.g: |exti12 (0)| exti13 (1)| exti14 (2)| exti15 (3)|
+	
+	SYSCFG->EXTICR[confReg] &= ~(0x0F << (confPos * 4));// each register is 4 bits wide hence 4 shifts first ~(0x00F0);
+	SYSCFG->EXTICR[confReg] |= port << (confPos * 4); 
+	
+	// Interuppt Mask Register (IMR)
+	uint32_t pinPos = 0x01UL << pin;
+	EXTI->IMR |= pinPos; // exti0, exti2, exti3...
+	
+	if(trigger_mode == 0)
+	{
+		// Rising Trigger Selection Register (RTSR)
+		EXTI->RTSR |= pinPos; // Rising edge triggered.
+	}	
+	else
+			{
+				// Falling Trigger Selection Register (FTSR)
+				EXTI->FTSR |= pinPos; // Falling edge triggered.
+			}
+		
+	uint8_t irqNum;
+  IRQn_Type EXTIx_IRQn;
+	
+	if(pin <= 4)
+	{
+		switch(pin)
+		{
+			case(0): irqNum = 6; 	EXTIx_IRQn = EXTI0_IRQn; break;
+			case(1): irqNum = 7; 	EXTIx_IRQn = EXTI1_IRQn; break;
+			case(2): irqNum = 8; 	EXTIx_IRQn = EXTI2_IRQn; break;
+			case(3): irqNum = 9; 	EXTIx_IRQn = EXTI3_IRQn; break;
+			case(4): irqNum = 10;	EXTIx_IRQn = EXTI4_IRQn; break;
+		}
+	}
+	else
+			if(pin >= 5 && pin <= 9)
+			{
+				irqNum = 23;
+				EXTIx_IRQn = EXTI9_5_IRQn; // EXTI_9_IRQn to EXTI_5_IRQn
+			}
+			else
+					if(pin >= 10 && pin <= 15)
+					{
+						irqNum = 40;
+						EXTIx_IRQn = EXTI15_10_IRQn; // EXTI_15_IRQn to EXTI_10_IRQn
+					}
+	
+	uint8_t extReg = irqNum / 32;		
+	uint8_t extPos = irqNum % 32;
+					
+	NVIC->ISER[extReg] = 0x01UL << (extPos);
+	NVIC_EnableIRQ(EXTIx_IRQn);
+}
+//------------------------------------------------------------------------------
+void EXTI9_5_IRQHandler(void)
+{
+	// External Interrupt (push button)
+  if(EXTI->PR & EXTI_PR_PR6)
+	{		
+		isTempMode = 1 - isTempMode; // Toggle state
+	}	
+	
+	EXTI->PR = EXTI_PR_PR6;
+}
+//------------------------------------------------------------------------------
+#endif
---
+++ dht22_exti_ht16k33_16seg/code/main.c
@@ -0,0 +1,38 @@
+//---------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "./assets/gpio/gpio_c.h"
+#include "./assets/delay/delay_c.h"
+#include "./assets/exti/extInterrupt_c.h"
+#include "./assets/bargraph/bargraph_c.h"
+#include "./assets/dht22/dht22_c.h"
+#include "./assets/ht16k33/ht16k33_c.h"
+//------------------------------------------------------------------------------
+dht22_t sensor;
+//------------------------------------------------------------------------------
+int main(void)
+{			
+	clock_init();	// Set Clock to 16Mhz	
+	ht16k33_init(15);   // Initialize HT16K33 with brightness level 15 
+	bargraph_init();
+	extInt_init(PA6, RISING_EDGE);
+
+	while(1)
+	{ 
+		if(dht22_getSensorData(&sensor))
+		{
+			if(isTempMode == 0)
+				ht16k33_print("%7.1f%c", sensor.humidity, '%');
+			if(isTempMode == 1)
+				ht16k33_print("%6.1f°C", sensor.temperature);
+	
+			bargraph_write((int)sensor.humidity);
+		}
+		else
+				ht16k33_print("%8s", "Error");
+		
+		delay_ms(1000);
+	}
+}
+//------------------------------------------------------------------------------
