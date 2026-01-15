6ed213f4e0f6e60d763c65a9b04bb27c93a51591
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-15 10:47:03 +0200

    Add files via upload

---
+++ keypad_4x3_pcf8574_ht16k33_16seg/code/assets/i2c/i2c_h.h
@@ -0,0 +1,24 @@
+#ifndef I2C_H
+#define I2C_H
+//------------------------------------------------------------------------------
+#include <stdint.h>
+#include "../gpio/gpio_c.h"
+//------------------------------------------------------------------------------
+#define SCL  PB8 
+#define SDA  PB9
+//------------------------------------------------------------------------------
+#define ACK  0
+#define NACK 1
+//------------------------------------------------------------------------------ 
+void i2c_init(void); 
+void i2c_start(void); 
+void i2c_restart(void);
+void i2c_stop(void); 
+void i2c_ack(uint8_t ackn); 
+void i2c_address(uint8_t address);
+void i2c_wait(void);
+uint8_t i2c_write(uint8_t data);
+uint8_t i2c_read(uint8_t ackState);
+//------------------------------------------------------------------------------
+#endif
+
