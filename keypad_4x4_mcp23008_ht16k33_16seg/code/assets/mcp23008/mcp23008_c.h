59e6c21d800023bf17498ed2ae3419cee7814938
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-15 10:39:00 +0200

    Add files via upload

---
+++ keypad_4x4_mcp23008_ht16k33_16seg/code/assets/mcp23008/mcp23008_c.h
@@ -0,0 +1,41 @@
+#ifndef MCP23008_C
+#define MCP23008_C
+//------------------------------------------------------------------------------
+#include "mcp23008_h.h"            
+//------------------------------------------------------------------------------
+void mcp23008_init(uint8_t devAddr, uint8_t portState)
+{
+	i2c_init();		// I2C1 selected
+
+	mcp23008_write(devAddr, IODIR, portState);   
+}
+//------------------------------------------------------------------------------
+void mcp23008_write(uint8_t devAddr, uint8_t regAddr, uint8_t data)
+{
+  i2c_start();
+ 
+  i2c_write(devAddr | 0);		// WRITE 
+  i2c_write(regAddr);
+	i2c_write(data);
+ 
+  i2c_stop();
+}
+//------------------------------------------------------------------------------
+uint8_t mcp23008_read(uint8_t devAddr)
+{
+  uint8_t data = 0;
+
+  i2c_start();
+  
+	i2c_write(devAddr | 0);		// WRITE 
+	i2c_write(GPIO);
+	i2c_restart();
+	i2c_write(devAddr | 1);		// READ
+	data = i2c_read(NACK);
+  
+  i2c_stop();
+  
+  return data;
+}
+//------------------------------------------------------------------------------
+#endif
