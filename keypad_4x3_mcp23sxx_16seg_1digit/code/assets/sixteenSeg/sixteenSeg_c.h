ff356fb461af105875af44ca04f56cc2b5b88c69
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-15 09:14:55 +0200

    Add files via upload

---
+++ keypad_4x3_mcp23sxx_16seg_1digit/code/assets/sixteenSeg/sixteenSeg_c.h
@@ -0,0 +1,41 @@
+#ifndef SIXTEEN_SEGMENT_C
+#define	SIXTEEN_SEGMENT_C
+//------------------------------------------------------------------------------
+#include "sixteenSeg_h.h"
+//------------------------------------------------------------------------------
+void sixteenSeg_init(void)
+{
+	mcp23s17_init(DIGIT_ADDR, 0x00, 0x00);
+	
+	sixteenSeg_char(' ');
+}
+//------------------------------------------------------------------------------
+void sixteenSeg_char(char chr) 
+{
+	uint16_t digit = getDigit(chr);
+	serial_digitSend(digit);  // charset is for common anode, thus inversion of byte to conform to common cathode
+}
+//------------------------------------------------------------------------------
+//													ascii to 16 segment
+//------------------------------------------------------------------------------
+uint16_t getDigit(char chr)
+{
+	uint16_t digit = 0;
+	
+	digit = (isPrint(chr)) ? chars[chr - ' '] : custom_chars[0]; // custom char is ° symbol
+					
+	return digit;
+}
+//------------------------------------------------------------------------------
+inline uint8_t isPrint(char chr)
+{
+	return (chr >= 32 && chr <= 127) ? 1 : 0; //Standard ascii range 32 - 127 for printable chars 
+}
+//------------------------------------------------------------------------------
+void serial_digitSend(uint16_t digit)
+{ 
+	mcp23s17_write(DIGIT_ADDR, OLAT_A, digit);  			// LSByte
+	mcp23s17_write(DIGIT_ADDR, OLAT_B, digit >> 8);		// MSByte
+}
+//------------------------------------------------------------------------------
+#endif
